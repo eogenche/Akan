983496883d7fee52c47a609deb3559fa8d0394d0
monte <eogenche@yahoo.com>
2020-02-14 14:07:21 +0300

    add js

---
+++ index.js
@@ -0,0 +1,13 @@
+function akan(){
+  var start = document.getElementById("dob").value;
+  var array = str.split(" ");
+  var mm = array.slice(0,2);
+  var dd = array.slice(2,4);
+  var cc = array.slice(4,6);
+  var yy = array.slice(6,8);
+
+  alert(start);
+
+  
+}
+
