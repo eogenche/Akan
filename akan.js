e5ad6bff6ecc74c502cd0ebfd722acaa791c1ecf
monte <eogenche@yahoo.com>
2020-02-16 13:32:16 +0300

    add new date correction

--- akan.js
+++ akan.js
@@ -1,3 +1,34 @@
-function myFunction() {
-  document.body.style.backgroundImage = "url('akan.jpg')";
+
+
+var verifiermale;
+var verifierfemale;
+var maleakan; 
+var femaleakan;
+var femname; 
+var malename;
+
+
+//var con = start.toDateString();
+//var dd = start.getDay();
+//var mm = start.getMonth();
+//var yy = start.getFullYear();
+
+
+function akan(){
+
+var femname = ("Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama");
+var malename = ("Kwasi","Kwadwo","Kwabena,","Kwaku","Yaw","Kofi","kwame");
+var start = new Date(document.getElementById("dob").value);
+//if(con === ""){
+ //   alert("enter Date Of Birth");
+  //}else{
+  
+  //}
+  
+
+  alert(start);
+  // alert(mm);
+  // alert(yy);
+  // -->
 }
+
