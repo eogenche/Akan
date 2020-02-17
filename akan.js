1b996f26a8e4f9f3cf4cbad4ce3d9adfa29effc5
monte <eogenche@yahoo.com>
2020-02-17 10:38:55 +0300

    add validation

--- akan.js
+++ akan.js
@@ -1,58 +1,46 @@
-
-
-var verifiermale;
-var verifierfemale;
+var male;
+var female;
 var maleakan; 
 var femaleakan;
 var femname; 
 var malename;
+var myinput;
 
-
-//
-//
-//
-//
-
-
+function noinput(){
+    myinput = document.getElementById("dob").value;
+    if(myinput===""){
+        alert("You must enter a date!");
+    }else{
+        alert("Date received!");
+    }
+}
 function akan(){
-
-var femname = ("Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama");
-var malename = ("Kwasi","Kwadwo","Kwabena,","Kwaku","Yaw","Kofi","kwame");
+noinput();
+var malename = ["Kwasi","Kwadwo","Kwabena,","Kwaku","Yaw","Kofi","kwame"];
+var femname = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var list = document.getElementById("list").value;
 var start = new Date(document.getElementById("dob").value);
 var con = start.toDateString();
-var dd = start.getDay();
-var mm = start.getMonth();
-var yy = start.getFullYear();
-//if(con === ""){
- //   alert("enter Date Of Birth");
-  //}else{
-  
-  //}
-  
+ var dd = start.getDay();
+// var mm = start.getMonth();
+// var yy = start.getFullYear();
+    
 
-  alert(yy);
-  // alert(mm);
-  // alert(yy);
-  // -->
-}
-function verifying(){
-  if(verifiermale=true){
-    maleakan = malename[dd];
-    alert(maleakan);
-  }else if(verifiermale = false){
-    verifiermale=true;
-  }else if(verifierfemale = true){
-    femaleakan=femname[dd];
-      alert(femaleakan);
-  }else {
-    verifiermale=true;
-    alert("enter names");
-  }
-}
-function maleclicked(){
-  verifiermale = true;
-}
-function femaleclicked(){
-  verifierfemale = true;
-
-}
+    if(list == "male"){
+        maleakan = malename[dd];
+        alert(maleakan);
+    // //  }else if(verifiermale = false){
+    // //    verifierfemale=true;
+    }else if(list == "female"){
+        femaleakan=femname[dd];
+          alert(femaleakan);
+    }else {
+        list = "male";
+    }
+ }
+// function maleclicked(){
+//   verifiermale = true;
+// }
+// function femaleclicked(){
+//    verifierfemale = true;
+// }
